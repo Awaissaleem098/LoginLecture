8bdf6609c6dd23c4ca9a2678a9d108b4394e2a82
fxgnmo <fbrjsah0725@gmail.com>
2023-08-16 00:21:59 +0900

    DOM Update

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,1 +1,13 @@
-console.log('s')
+const id = document.querySelector('#id');
+const psword = document.querySelector('#psword');
+const loginBtn = document.querySelector("button")
+
+const login = () => {
+    const req = {
+        id: id.value,
+        psword: psword.value,
+    };
+    console.log(req)
+}
+
+loginBtn.addEventListener('click', login);
